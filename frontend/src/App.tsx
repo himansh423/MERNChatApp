d1c5ab0cb5d8dc7bea16763fd83a6f05497b67cb
Himanshu Chauhan <himanshuchau423@gmail.com>
2024-07-23 16:51:43 +0530

    created signup page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,8 @@ import {
 const Navbar = lazy(() => import("./components/Header.tsx"));
 const HomeScreen = lazy(() => import("./pages/HomeScreen"));
 const ChatRoom = lazy(() => import("./components/ChatRoom.tsx"));
-const Login = lazy(() => import("./components/Login.tsx"))
+const Login = lazy(() => import("./components/Login.tsx"));
+const SignUp = lazy(() => import("./components/SignUp.tsx"));
 const AppContent = () => {
   const location = useLocation();
   const isChatRoom = location.pathname.startsWith("/chatroom/");
@@ -21,6 +22,7 @@ const AppContent = () => {
         <Route path="/" element={<HomeScreen />} />
         <Route path="/chatroom/:id" element={<ChatRoom />} />
         <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<SignUp/>} />
       </Routes>
     </>
   );
--- frontend/src/components/SignUp.tsx
+++ frontend/src/components/SignUp.tsx
@@ -3,13 +3,13 @@ import { FaGithub } from "react-icons/fa";
 import styles from "./Login.module.css";
 import { Link } from "react-router-dom";
 
-const Login = () => {
+const SignUp = () => {
   return (
     <main className="bg-black py-4 h-screen w-screen text-white">
       <div className="flex justify-center flex-col">
-        <h1 className={`${styles.head} text-center font-bold`}>Login</h1>
+        <h1 className={`${styles.head} text-center font-bold`}>Sign up</h1>
         <p className="text-center mt-2 text-gray-600 text-[13px]">
-          Login with One Of the Following options.
+          Sign-up with One Of the Following options.
         </p>
         <div className="flex w-screen p-3 gap-3 mt-3">
           <button
@@ -25,13 +25,24 @@ const Login = () => {
         </div>
 
         <form className="flex flex-col items-center mt-7 px-5">
-          <div className="flex flex-col gap-2 w-full ">
+        <div className="flex flex-col gap-2 w-full ">
+            <label htmlFor="name" className="px-1">Name*</label>
+            <input
+              type="text"
+              className={`${styles.input} h-14 w-full border border-gray-400 bg-[#171717] px-3 py-2 rounded-md`}
+              name="name"
+              id="name"
+              autoComplete="off"
+            />
+          </div>
+          <div className="flex flex-col gap-2 w-full mt-4">
             <label htmlFor="email" className="px-1">Email*</label>
             <input
               type="email"
               className={`${styles.input} h-14 w-full border border-gray-400 bg-[#171717] px-3 py-2 rounded-md`}
               name="email"
               id="email"
+              autoComplete="off"
             />
           </div>
           <div className="flex flex-col gap-2 w-full  mt-4">
@@ -41,18 +52,19 @@ const Login = () => {
               className={`${styles.input} h-14 w-full border border-gray-400 bg-[#171717] px-3 py-2 rounded-md`}
               name="password"
               id="password"
+              autoComplete="off"
             />
           </div>
 
-          <button className={`${styles.SubmitButton} mt-7 w-full h-14 bg-purple-600  text-white py-2 px-4 rounded`}>Login</button>
+          <button className={`${styles.SubmitButton} mt-7 w-full h-14 bg-purple-600  text-white py-2 px-4 rounded`}>Sign up</button>
         </form>
 
         <p className="text-center mt-6">
-          Don't have an account? <Link to={'/signup'} className="text-blue-500 cursor-pointer">Sign up</Link>
+          Already have an account? <Link to={'/login'} className="text-blue-500 cursor-pointer">Login</Link>
         </p>
       </div>
     </main>
   );
 };
 
-export default Login;
+export default SignUp;
